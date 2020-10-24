a57c8e63c86d14c267529b1b1824ff4c2669dbc7
AnsonAMS <anson_tohata@hotmail.com>
2020-10-24 19:55:46 +0200

    Updating script

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+const wordEl = document.getElementById('word');
+const wordlettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-again');
+const popup = document.getElementById('popup-container');
+const notifaction = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
